ed947195cad0a27cdaf35be5f908bce46945fba9
praveenbabud <andpraveen@gmail.com>
2018-03-24 22:29:17 +0530

    one more try

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-var express = require('express');
-var app = express();
+var http = require('http');
 
-var port = 4999;
+var server = http.createServer(function(request, response) {
 
-app.listen(port, function(err){
-    console.log('running server on port ' + port);
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
 });
 
-console.log("Hello");
+var port = process.env.PORT || 1337;
+server.listen(port);
 
-app.get('/', function(req, res){
-    res.send('Hello World');
-});
-
-app.get('/books', function(req, res){
-    res.send('Hello World Books');
-});
+console.log("Server running at http://localhost:%d", port);
